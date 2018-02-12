616e615d6ff0d8e0c24eb0a647dcd6342487b4b3
rishabh-malik <rishabhmalik249@gmail.com>
2018-02-12 20:25:04 +0530

    firebase

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
 var app = express();
+var firebase = require('firebase');
+var app = firebase.initializeApp({ 
+  apiKey: "AIzaSyDqTKSOn-SOQ_PMYH4icT5LfswQFwPqiUM",
+  authDomain: "hackathon-4febb.firebaseapp.com",
+  databaseURL: "https://hackathon-4febb.firebaseio.com",
+  projectId: "hackathon-4febb",
+  storageBucket: "hackathon-4febb.appspot.com",
+  messagingSenderId: "22239613539"  
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.render("index");
+app.get("/:id", function(req,res){
+  var userQuery=firebase.database().ref('users');
+  const query=userQuery.orderByChild('name').equalTo(req.params.id);
+  query.on('value',snap=>{
+      console.log(snap.val());
+      user=snap.val();
+      res.render('index',{user:user})
+  });	
 });
 
 //Deploy app on server
